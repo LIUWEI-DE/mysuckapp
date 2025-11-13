b7eea91fb359bd5086f49c60a85d828d0ff708e3
LIUWEI-DE <b11303038@mail.ntust.edu.tw>
2025-11-13 10:04:44 +0800

    Update2 app.py

--- app.py
+++ app.py
@@ -17,11 +17,6 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-# 確保資料表在第一次請求前建立
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 # 首頁：顯示所有文章
 @app.route('/')
 def index():
@@ -47,9 +42,9 @@ def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', post=post)
 
-if __name__ == '__main__':
-    # 本地開發用
-    with app.app_context():
-        db.create_all()  # 建立資料庫
-    app.run(debug=True)
+# 用 app context 初始化資料庫
+with app.app_context():
+    db.create_all()
 
+if __name__ == '__main__':
+    app.run(debug=True)

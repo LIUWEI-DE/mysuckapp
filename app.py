6fdc7157e8646ff593056bbd93759c9b023cdcab
b11303038@mail.ntust.edu.tw <b11303038@mail.ntust.edu.tw>
2025-11-13 08:03:53 +0800

    first commitdir

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+# 設定 SQLite 資料庫
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# 定義資料庫模型
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+# 首頁：顯示所有文章
+@app.route('/')
+def index():
+    posts = Post.query.order_by(Post.date_created.desc()).all()
+    return render_template('index.html', posts=posts)
+
+# 新增文章頁面
+@app.route('/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        content = request.form['content']
+        new_post = Post(title=title, author=author, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('new_post.html')
+
+# 檢視單篇文章
+@app.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', post=post)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # 建立資料庫
+    app.run(debug=True)
